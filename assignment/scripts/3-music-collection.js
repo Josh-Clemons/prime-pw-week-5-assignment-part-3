7d137bfe0ed8abedf5af0583d26c7084d8f5c072
Josh Clemons <mrjoshc@gmail.com>
2022-10-18 12:02:22 -0500

    Updated 'tracks' stretch goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,14 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection (title, artist, yearPublished) {
+function addToCollection (title, artist, yearPublished, tracks) {
     let album = {
         title: title,
         artist: artist,
-        yearPublished: yearPublished
+        yearPublished: yearPublished,
+        tracks: tracks
     }
-    collection.push (album)
+    collection.push (album);
     return album;
 } //end addToCollection
 
@@ -15,7 +16,7 @@ function findByArtist ( artist ) {
     let match = [];
     for ( i=0; i<collection.length; i++) {
         if ( artist===collection[i].artist ) {
-            match.push( collection[i].title );
+            match.push( collection[i] );
             console.log( 'added album to match:', collection[i].title );
         }
     }
@@ -25,12 +26,18 @@ function findByArtist ( artist ) {
 function search (arg) {
     let match = [];
     for ( i=0; i<collection.length; i++ ) {
-        if ( arg == null) {
+        if ( arg == null) { // if no value entered then returns entire collection
             return collection;
-        } else if (arg.artist == collection[i].artist && arg.year == collection[i].yearPublished) {
+        } else if (arg.artist == collection[i].artist && arg.year == collection[i].yearPublished) { //returns all albums with matching artist and yearPublished
             match.push (collection[i] );
-        } else if ( (arg.artist == "" || arg.artist == null) && (arg.year == "" || arg.year == null ) ) {
+        } else if ( arg.artist == "" && arg.year == "" ) { // if empty artist and year string is entered, returns collection
             return collection;
+        } else if (arg.tracks) { // returns albums where the track name matches
+            for ( k=0; k<collection[i].tracks.length; k++ ) {
+                if ( arg.tracks.name === collection[i].tracks[k].name ) {
+                    match.push( collection[i]);
+                }
+            }
         }
     }
     return match;
@@ -40,16 +47,19 @@ function search (arg) {
 function showCollection ( collectionArray ) {
     console.log( 'Collection length:' , collectionArray.length );
     for (i=0; i<collectionArray.length; i++) {
-        console.log( collectionArray[i].title, 'by', collectionArray[i].artist, 'published in', collectionArray[i].yearPublished );
+        console.log( collectionArray[i].title, 'by', collectionArray[i].artist, 'published in', collectionArray[i].yearPublished + ':' );
+        for ( k=0; k<collection[i].tracks.length; k++ ) {
+            console.log( k+1 + '. NAME:', collection[i].tracks[k].name, collection[i].tracks[k].duration );
+        }
     }
 } //end showCollection
 
-console.log( 'added', addToCollection ('title1', 'artist1', 1999), 'to collection' );
-console.log( 'added', addToCollection ('title2', 'artist1', 2000), 'to collection' );
-console.log( 'added', addToCollection ('title3', 'artist1', 1999), 'to collection' );
-console.log( 'added', addToCollection ('title4', 'artist2', 1979), 'to collection' );
-console.log( 'added', addToCollection ('title5', 'artist2', 1699), 'to collection' );
-console.log( 'added', addToCollection ('title6', 'artist3', 1959), 'to collection' );
+console.log( 'added', addToCollection ('title1', 'artist1', 1999, [{name: 'track1', duration: '42:13'}, {name: 'track2', duration: '2:33'}, {name: 'track3', duration: '4:13'}] ), 'to collection' );
+console.log( 'added', addToCollection ('title2', 'artist1', 2000, [{name: 'back1', duration: '2:13'}, {name: 'back2', duration: '1:33'}, {name: 'back3', duration: '5:13'}] ), 'to collection' );
+console.log( 'added', addToCollection ('title3', 'artist1', 1999, [{name: 'pack1', duration: '12:13'}, {name: 'crack2', duration: '7:33'}, {name: 'crack3', duration: '1:13'}] ), 'to collection' );
+console.log( 'added', addToCollection ('title4', 'artist2', 1979, [{name: 'stack1', duration: '2:34'}, {name: 'stack2', duration: '9:33'}, {name: 'stack3', duration: '3:33'}] ), 'to collection' );
+console.log( 'added', addToCollection ('title5', 'artist2', 1699, [{name: 'knack1', duration: '2:56'}, {name: 'knack2', duration: '12:33'}, {name: 'knack3', duration: '2:23'}] ), 'to collection' );
+console.log( 'added', addToCollection ('title6', 'artist3', 1959, [{name: 'pack1', duration: '2:51'}, {name: 'pack2', duration: '22:33'}, {name: 'pack3', duration: '8:13'}] ), 'to collection' );
 console.log( 'current collection:', collection )
 
 showCollection (collection);
@@ -60,3 +70,6 @@ console.log( 'Testing findByArtist, should return empty array: ', findByArtist( 
 console.log( 'Searching for Ray Charles albums from 1957, returning results:', search ({ artist: 'Ray Charles', year: 1957 } ) );
 console.log( 'Searching for albums by artist1 in year 1999:', search ({ artist: 'artist1', year: 1999 } ) );
 console.log( 'Searching empty search object:', search () );
+console.log( 'Search by track name, returning albums with tracks named "pack1":', search( {tracks: {name: 'pack1'}} ) );
+
+
